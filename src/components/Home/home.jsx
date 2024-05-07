7832a19956936b596a4e49b1d8968937fb531849
Patyadl <patricia.00000848012@unicap.br>
2024-05-06 22:54:06 -0300

    section de dicas parcialmente estilizada

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -24,11 +24,43 @@ const Home = () =>{ return (
          <div className={"imgPc"}>
          </div>
          <div className={"frases"}>
-            <h1>WayClient <p>- espera e + vendas, totalmente automático.</p></h1>
+            <h1>WayClient <p><span>-</span> espera e <span>+</span> vendas, totalmente <br></br> automático.</p></h1>
          </div>
               
+              
        </section>
-       
+
+      <section className={"dicas-content"}>
+         <div className={"dicas"}>
+         <h2>Por que o WayClient?</h2>
+
+
+        <div className={"caixas-content"}>
+         <div id="caixas">
+            <div id={"icon-1"}></div>
+            
+           <h4>Melhorar seu atendimento</h4>
+           <br />
+           <p>Maximize a eficiência operacional, melhore a satisfação do seu cliente e 
+           impulsione o crescimento empresarial.</p>
+         </div>
+         <div id="caixas">
+         <div id={"icon-2"}></div>
+            <h4>Personalização</h4>
+            <br />
+           <p> Ofereça uma experiência personalizada  ao cliente, aumentando sua satisfação e <br /> fidelidade.</p>
+         </div>
+         <div id="caixas">
+         <div id={"icon-3"}></div>
+            <h4>Eficiência</h4>
+            <br />
+            <p>Automatiza processos, reduzindo <br /> tempo e esforço na gestão de <br /> tickets e solicitações.</p>
+         </div>
+         </div>
+
+         </div>
+          
+         </section>       
     </div>
 )}
 
