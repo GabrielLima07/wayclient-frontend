03949515113a3d95c4cdb55503eaef0522b32843
Giovanna Cunha <giovanna.00000847950@unicap.br>
2024-05-14 12:58:56 -0300

    iniciando página Cadastro de Colaborador

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/Home/home'
+import CadastroColaborador from './components/CadastroColaborador/cadastroColaborador'
 import App from './App'
 
 function Main() {
@@ -8,6 +9,7 @@ function Main() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/cadastroColaborador" element={<CadastroColaborador />} />
         <Route path="/app" element={<App />} />
        
       </Routes>
