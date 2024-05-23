ead86b1ffb8ada4014bc2aaf4d8f1f653c7bc9a1
Antony Menezes <makmenezes2@gmail.com>
2024-05-23 00:06:11 -0300

    atualização menu funcionario

--- src/components/funcionario/menufuncionario.jsx
+++ src/components/funcionario/menufuncionario.jsx
@@ -1,31 +1,48 @@
+import React, { useState } from 'react';
+import './menufuncionario.css';
 
-import './menufuncionario.css'
+const Menufuncionario = () => {
+  const userData = {
+    name: "Filipe Granja",
+    imagem: "../../assets/funcionario/perfil.png",
+  };
 
-const Menufuncionario = ()=>{
-      const userData = {
-        name:"Filipe Granja",
-        imagem:"../../assets/funcionario/perfil.png" ,
-    }
-    return(
-        <div>
-            <nav className={"menu-content-funcionario"}> 
-             <div className={"menu-funcionario"}>
-                <h2 id={"logo"}>WayClient</h2>
-               
-               <div className={"textos"}>
-                <h3 >Tickets</h3>
-                <h3 >Mensagens</h3>
-                <h3>Histórico</h3>
-                <input type="text" placeholder="pesquisar"></input>
-                </div>
-                <div className='funcio'>
-                    <div className="imgPerfil"></div>
-                    <p className='nameFun'>{userData.name}</p>
-                </div>
-                    
-             </div>
-          </nav>
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!dropdownVisible);
+  };
+
+  return (
+    <div>
+      <nav className={"menu-content-funcionario"}>
+        <div className={"menu-funcionario"}>
+          <h2 id={"logo"}>WayClient</h2>
+          
+          <div className={"textos"}>
+            <h3>Tickets</h3>
+            <h3>Mensagens</h3>
+            <h3>Histórico</h3>
+            <input type="text" placeholder="Pesquisar"></input>
+          </div>
+          <div className='funcio'>
+            <div className="imgPerfil"></div>
+            <p className='nameFun'>{userData.name}</p>
+            <div className="dropdown-toggle" onClick={toggleDropdown}>
+              {dropdownVisible ? '▲' : '▼'}
+            </div>
+            {dropdownVisible && (
+              <div className="dropdown-menu">
+                <p className="dropdown-item">Perfil</p>
+                <hr />
+                <p className="dropdown-item">Logout</p>
+              </div>
+            )}
+          </div>
         </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
+
 export default Menufuncionario;
