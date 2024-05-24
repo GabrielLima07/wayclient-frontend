ad3139bc28c3794057a617066683346fb9c51440
Gabriel Lima <102686294+GabrielLima07@users.noreply.github.com>
2024-05-24 18:14:37 -0300

    feat: add navigate hook to login and signup pages

--- src/components/LoginPageNavbar/Navbar.jsx
+++ src/components/LoginPageNavbar/Navbar.jsx
@@ -1,20 +1,35 @@
 import React from 'react';
 import "./navbar.css";
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+  const navigate = useNavigate();
+
+  const handleSignupClick = () => {
+    navigate("/signup")
+  }
+
+  const handleLoginClick = () => {
+    navigate("/login")
+  }
+
+  const handleLogoClick = () => {
+    navigate("/")
+  }
+
   return (
     <div>
       <nav className={"menu-content"}> 
         <div className={"menu"}>
-            <h2 id={"logo"}>WayClient</h2>
+            <h2 id={"ls-logo"} onClick={handleLogoClick}>WayClient</h2>
 
             <div className={"textos"}>
-              <h3 >Central de Ajuda</h3>
-              <h3 >FAQ</h3>
+              <h3>Central de Ajuda</h3>
+              <h3>FAQ</h3>
               <h3>Contato</h3>
-              <h3 >Login</h3>
+              <h3 onClick={handleLoginClick}>Login</h3>
 
-              <h3 id={"cadastre"}>Cadastre-se</h3>
+              <h3 id={"cadastre"} onClick={handleSignupClick}>Cadastre-se</h3>
             </div>
           </div>
         </nav>
